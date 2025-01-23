cc9a4e18bd87af67e8a1a2f8fbc50a08bdcedb59
lakhanprajapat1 <lakhan10209@gmail.com>
2025-01-23 16:10:42 +0530

    complete Ludo Board Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import LudoBoard from './LudoBoard.jsx'
 function App() {
   return (
     <>
+      <h2>Ludo Board</h2>
       <LudoBoard/>
     </>
   )
--- src/LudoBoard.jsx
+++ src/LudoBoard.jsx
@@ -2,17 +2,37 @@ import React from 'react'
 import { useState } from 'react'
 
 const LudoBoard = () => {
-    let [move, setmove]=useState()
+    let [move, setmove]=useState({blue:0, yellow:0, green:0, red:0})
+    let updateBlue=()=> {
+      setmove((preVal)=> {
+        return{...preVal, blue:preVal.blue+1 }
+      })
+    }
+    let updateYellow=()=> {
+      setmove((preVal)=> {
+        return{...preVal, yellow:preVal.yellow+1 }
+      })
+    }
+    let updateGreen=()=> {
+      setmove((preVal)=> {
+        return{...preVal, green:preVal.green+1 }
+      })
+    }
+    let updateRed=()=> {
+      setmove((preVal)=> {
+        return{...preVal, red:preVal.red+1 }
+      })
+    }
   return (
     <div className='board'>
-      <p>Blue Moves =</p>
-      <button style={{backgroundColor:"blue"}}>+1</button>
-      <p>yellow Moves =</p>
-      <button style={{backgroundColor:"yellow", color:"black"}}>+1</button>
-      <p>Green Moves =</p>
-      <button style={{backgroundColor:"green"}}>+1</button>
-      <p>Red Moves =</p>
-      <button style={{backgroundColor:"red"}}>+1</button>
+      <p>Blue Moves = {move.blue}</p>
+      <button style={{backgroundColor:"blue"}} onClick={updateBlue}>+1</button>
+      <p>yellow Moves ={move.yellow}</p>
+      <button style={{backgroundColor:"yellow", color:"black"}} onClick={updateYellow}>+1</button>
+      <p>Green Moves ={move.green}</p>
+      <button style={{backgroundColor:"green"}} onClick={updateGreen}>+1</button>
+      <p>Red Moves = {move.red}</p>
+      <button style={{backgroundColor:"red"}} onClick={updateRed}>+1</button>
     </div>
   )
 }
