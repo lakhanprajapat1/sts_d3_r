05243f19afffe979e4793bbd97a2bbd7a9245e86
lakhanprajapat1 <lakhan10209@gmail.com>
2025-01-28 19:31:48 +0530

    almost done

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -4,12 +4,14 @@ import "./stylesheets/TodoList.css";
 import { v4 as uuidv4 } from "uuid";
 
 const TodoList = () => {
-  let [todos, settodos] = useState([{ task: "sample Task", id: uuidv4() }]);
+  let [todos, settodos] = useState([
+    { task: "Sample Task", id: uuidv4(), isDone: false },
+  ]);
   let [newTodo, setnewTodo] = useState("");
 
   let addNewTask = () => {
     settodos((preValue) => {
-      return [...preValue, { task: newTodo, id: uuidv4() }];
+      return [...preValue, { task: newTodo, id: uuidv4(), isTrue: false }];
     });
     setnewTodo("");
   };
@@ -22,21 +24,46 @@ const TodoList = () => {
     settodos((preTodos) => todos.filter((preTodos) => preTodos.id != id));
   };
 
-  let changeAll= ()=>{
-    
-  }
+  let changeAll = () => {
+    settodos(() =>
+      todos.map((todo) => {
+        return {
+          ...todo,
+          isDone: true,
+        };
+      })
+    );
+  };
+
+  let markAsDone = (id) => {
+    settodos((preVal) =>
+      preVal.map((todo) => {
+        if (todo.id == id) {
+          return {
+            ...todo,
+            isDone: true,
+          };
+        } else {
+          return todo;
+        }
+      })
+    );
+  };
+
   return (
-    <div>
-      <h1>TodoList</h1>
-      <label>
-        <input
-          type="text"
-          placeholder="Enter A Task"
-          value={newTodo}
-          onChange={updateTodoValues}
-        />
-      </label>
-      <button onClick={addNewTask}>Add Task</button>
+    <div className="container">
+      <h1 className="heading">TodoList</h1>
+      <div className="input-div">
+        <label>
+          <input
+            type="text"
+            placeholder="Enter A Task"
+            value={newTodo}
+            onChange={updateTodoValues}
+          />
+        </label>
+        <button className="add-task" onClick={addNewTask}>Add Task</button>
+      </div>
       <br></br>
       <br></br>
       <hr />
@@ -45,13 +72,33 @@ const TodoList = () => {
         {todos.map((todo) => {
           return (
             <li key={todo.id} type="none">
-              <span>{todo.task}</span>
+              <span
+                style={
+                  todo.isDone
+                    ? { textDecoration: "line-through", color: "gray" }
+                    : {}
+                }
+              >
+                {todo.task}
+              </span>
               &nbsp;
-              <button onClick={() => deleteTask(todo.id)}>Delete</button>
+              <button className="done-btn" onClick={() => markAsDone(todo.id)}>
+                {" "}
+                Done
+              </button>
+              <button
+                className="delete-btn"
+                onClick={() => deleteTask(todo.id)}
+              >
+                Delete
+              </button>
             </li>
           );
         })}
       </ul>
+      <button className="all-done-btn" onClick={changeAll}>
+        Mark All Done
+      </button>
     </div>
   );
 };
